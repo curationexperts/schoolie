5725db584c8a5b78e9346103cc8dd3730a9f540b
McClain Looney <mcclain@curationexperts.com>
2022-02-17 14:26:15 -0600

    Adds coveralls.yml, re-organizes coverage boot

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
-$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "schoolie"
-
-require "minitest/autorun"
 if ENV["COVERAGE"]
   require "simplecov"
   SimpleCov.start
   require "coveralls"
   Coveralls.wear!
 end
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "schoolie"
+
+require "minitest/autorun"
